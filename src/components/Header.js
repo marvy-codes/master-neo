22bfcf2148cf51a78fa0890021b9eea8b59e27e2
Olowookere Marvellous Oyedamola <olowomarve@gmail.com>
2021-11-24 13:34:28 +0100

    navBar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,25 @@ import Logo from "../images/Logo.svg";
 
 function Header() {
     
-    const [navbarOpen, setNavbarOpen] = useState(false)
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    const handleToggle = () => {
+        setNavbarOpen(prev => !prev)
+    };
+    // const closeMenu = () => {
+    //     setNavbarOpen(false)
+    // };
   
     return (
       <div>
         <header className="header">
 
-            <div>
-                <p>text</p>
-            </div>
+            <nav className="navBar">
+                 <button onClick={handleToggle} className="hamburger">{navbarOpen ? "Close" : "Open"}</button>
+                    <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
+                        ...
+                    </ul>
+            </nav>
+            
 
             <div className="header_Logo">
                 <img src={Logo} alt="Master neo" />
