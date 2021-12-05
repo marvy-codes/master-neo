fb6565a91743fe3289720540e37957916197d30e
Olowookere Marvellous Oyedamola <olowomarve@gmail.com>
2021-12-05 02:24:35 +0100

    complete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,30 @@
 import '../css/Footer.css';
 import footermlogo from "../images/footermlogo.svg";
+import twitterlogo from "../images/twitterlogo.svg";
+import instagramlogo from "../images/instagramlogo.svg";
+import mediumlogo from "../images/mediumlogo.svg";
 
 function Footer() {
   return (
     <div>
-        <div className="Footer">
-            <div>
+        <div className="footer">
+            <div className="foot1">
                 <h5><span className="MNMA">want to work us?</span></h5>
-                <button className="button">Hire us</button>
-                <h5><span className="MNMA">Quick Links</span></h5>
-                <ul>
+                <button className="button footerbutton">Hire us</button>
+                <h4><span className="MNMA">Quick Links</span></h4>
+                <ul className="footernav">
                     <li>About Us</li>
                     <li>Why US</li>
                     <li>Clients</li>
                     <li>Contact Us</li>
                 </ul>
+                <ul  className="footerul">
+                    <li><img src={twitterlogo} alt="twitter" className="twitterlogo" /></li>
+                    <li><img src={mediumlogo} alt="medium" className="mediumlogo" /></li>
+                    <li><img src={instagramlogo} alt="instagram" className="instagramlogo" /></li>
+                </ul>
             </div>
-            <div>
+            <div className="foot2">
                 <img src={footermlogo} alt="Master Logo" className="footerlogo" />
             </div>
         </div>
