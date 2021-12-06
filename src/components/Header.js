7bca05a6d3e4c0d06f2abee23901fe4e813080d6
Olowookere Marvellous Oyedamola <olowomarve@gmail.com>
2021-12-06 20:56:36 +0100

    header desktop mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,9 @@ import { FiMenu } from "react-icons/fi";
 import '../css/Header.css';
 import Logo from "../images/Logo.svg";
 import navbarlogo from "../images/navbarlogo.svg";
+import twitterlogo from "../images/twitterlogo.svg";
+import instagramlogo from "../images/instagramlogo.svg";
+import mediumlogo from "../images/mediumlogo.svg";
 
 function Header() {
     
@@ -18,7 +21,6 @@ function Header() {
     return (
       <div>
         <header className="header">
-
             <nav className="navBar">
                  <button onClick={handleToggle}>{navbarOpen ? (
                         <MdClose style={{ color:"#FCFCFC", width: "40px", height: "40px" }} />
@@ -35,8 +37,6 @@ function Header() {
                     <button className="button navbutton" onClick={() => closeMenu()}>Hire us </button>
                 </ul>
             </nav>
-            
-
             <div className="header_logo">
                 <img src={Logo} alt="Master neo" />
             </div>
@@ -44,6 +44,32 @@ function Header() {
             <div>
                 <button className="button">
                     Hire us
+                </button>
+            </div>
+        </header>
+
+        <header className="header__desktop">
+
+            <div className="header__desktop__item1">
+                <img src={Logo} alt="Master neo" />
+            </div>
+
+            <div className="header__desktop__item2">
+                <ul className="menuNav__desktop1">
+                    <li>About us</li>
+                    <li>Why us</li>
+                    <li>Clients</li>
+                </ul>
+            </div>
+
+            <div className="header__desktop__item3">
+                <ul className="menuNav__desktop2"> 
+                    <li><img src={twitterlogo} alt="twitter" className="twitterlogo" /></li>
+                    <li><img src={mediumlogo} alt="medium" className="mediumlogo" /></li>
+                    <li><img src={instagramlogo} alt="instagram" className="instagramlogo" /></li>
+                </ul>
+                <button className="button">
+                        Hire us
                 </button>
             </div>
 
