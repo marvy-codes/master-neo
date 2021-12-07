8ab73769a894ccedaa5f834c97fd182ba2583f56
Olowookere Marvellous Oyedamola <olowomarve@gmail.com>
2021-12-07 01:29:14 +0100

    complete about desktop

--- src/components/Whatcomponent.js
+++ src/components/Whatcomponent.js
@@ -11,56 +11,67 @@ import abouticon from "../images/Abouticon.svg";
 function Whatcomponent() {
   return (
     <div>
-      <div className="What">
-        <h5>What is <span className="MNMA">MNMA</span>?</h5>
-        <p>
-              Master Neo's Moderation Agency is a platform that deals in the managing and handling 
-              of social apps and the community of crypto inclined Companies and projects. You've
-              got a project and you need Mods for a chat or Twitter handlers?, well search no more 
-              cause you're in the right place. We offer various managerial services, such as:
-        </p>
-        <div className="whatcards">
-              <div className="whatcard">
-                  <img src={whatcard1} alt="Fully Doxxed Moderation" className="whatcard1" />
-                  <div className="whatglowDot"></div>
-              </div>
-              <div className="whatcard">
-                  <img src={whatcard2} alt="Multi App Management" className="whatcard2" />
-                  <div className="whatglowDot"></div>
-              </div>
-              <div className="whatcard">
-                  <img src={whatcard3} alt="community Focus" className="whatcard3" />
-                  <div className="whatglowDot"></div>
-              </div>
+      <div className="what">
+        <div className="what__desktop1">
+            <h5>What is <span className="MNMA">MNMA</span>?</h5>
+            <p>
+                  Master Neo's Moderation Agency is a platform that deals in the managing and handling 
+                  of social apps and the community of crypto inclined Companies and projects. You've
+                  got a project and you need Mods for a chat or Twitter handlers?, well search no more 
+                  cause you're in the right place. We offer various managerial services, such as:
+            </p>
+            <div className="whatcards">
+                  <div className="whatcard">
+                      <img src={whatcard1} alt="Fully Doxxed Moderation" className="whatcard1" />
+                      <div className="whatglowDot"></div>
+                  </div>
+                  <div className="whatcard">
+                      <img src={whatcard2} alt="Multi App Management" className="whatcard2" />
+                      <div className="whatglowDot"></div>
+                  </div>
+                  <div className="whatcard">
+                      <img src={whatcard3} alt="community Focus" className="whatcard3" />
+                      <div className="whatglowDot"></div>
+                  </div>
+            </div>
         </div>
-        <div className="whatimage">
-          <img src={telegramicon} alt="telegram" className="whatimage__telegram" />
-          <img src={twittericon} alt="twtter" className="whatimage__twitter" />
-          <img src={discordicon} alt="discord"  className="whatimage__discord" />
+        
+        <div className="what__desktop2">
+              <div className="whatimage">
+                <img src={telegramicon} alt="telegram" className="whatimage__telegram" />
+                <img src={twittericon} alt="twtter" className="whatimage__twitter" />
+                <img src={discordicon} alt="discord"  className="whatimage__discord" />
+              </div>
         </div>
       </div>
 
-      <div className="About">
-          <h5>About <span className="MNMA">MNMA</span></h5>
-            <p>
-              Master Neo's Moderation Agency is fully sustainable and efficient Moderation provision
-              service that gives Project owners and Developers confidence and peace of mind knowing
-              that their communities and social Accounts are properly and efficiently managed free 
-              from spammers fudders and negative individuals.
-            </p>
-            <p>
-              MNMA is a revolutionary approach to the management Space that matches all your needs with
-              an all-in-one-robust, easy-to-apply platform.
-            </p>
-            <p>
-              We offer 24-hour coverage on community groups. We have Moderators from every Time-Zone needed
-              to achieve proper 24-hour community coverage. This directly increases effective management of
-              project groups and chats, in which we have one or two people covering chat at every hour of the
-              day handling FUDS, SPAMS and NEGATIVE TALKS.
-            </p>
-          <div className="Aboutimage">
-            <img src={abouticon} alt="about description" className="Abouticon" />
-          </div>
+      <div className="about__desktop">
+        <div className="about">
+            <div className="about__desktop1">
+              <h5>About <span className="MNMA">MNMA</span></h5>
+                <p>
+                  Master Neo's Moderation Agency is fully sustainable and efficient Moderation provision
+                  service that gives Project owners and Developers confidence and peace of mind knowing
+                  that their communities and social Accounts are properly and efficiently managed free 
+                  from spammers fudders and negative individuals.
+                </p>
+                <p>
+                  MNMA is a revolutionary approach to the management Space that matches all your needs with
+                  an all-in-one-robust, easy-to-apply platform.
+                </p>
+                <p>
+                  We offer 24-hour coverage on community groups. We have Moderators from every Time-Zone needed
+                  to achieve proper 24-hour community coverage. This directly increases effective management of
+                  project groups and chats, in which we have one or two people covering chat at every hour of the
+                  day handling FUDS, SPAMS and NEGATIVE TALKS.
+                </p>
+            </div>
+            <div className="about__desktop2">
+              <div className="aboutimage">
+                <img src={abouticon} alt="about description" className="abouticon" />
+              </div>
+            </div>
+        </div>
       </div>
     </div>
   );
